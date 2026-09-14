74605a3297302124183078b042ae3a363b8d9316
Pepijn de Vos <pepijndevos@gmail.com>
2026-09-14 12:33:15 +0200

    Install CI test plotting and Windows loader deps

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -25,13 +25,13 @@ case "$OS" in
         cmake ninja-build \
         bison flex flex-devel \
         suitesparse-devel \
-        openblas-devel lapack-devel \
-        llvm-devel clang clang-devel
+         openblas-devel lapack-devel \
+         llvm-devel clang clang-devel
       # The simulator spawns "python3" from PATH at runtime (see libplatform.cpp)
       # to run test control blocks that import numpy/scipy. AlmaLinux's python3
       # has no pip in this image, so bootstrap it with ensurepip first.
       python3 -m ensurepip --upgrade
-      python3 -m pip install --upgrade numpy scipy
+       python3 -m pip install --upgrade numpy scipy matplotlib
       # Boost bootstrap uses gcc (the manylinux default compiler). clang is also
       # installed above, but only because openvaf-r's build requires it.
       BOOST_TOOLSET=gcc
@@ -45,7 +45,7 @@ case "$OS" in
         libsuitesparse-dev \
         libopenblas-dev liblapack-dev \
         llvm-dev libclang-dev clang \
-        python3 python3-numpy python3-scipy
+         python3 python3-numpy python3-scipy python3-matplotlib
       BOOST_TOOLSET=clang
     fi
 
@@ -79,7 +79,7 @@ case "$OS" in
       bison flex \
       suite-sparse openblas boost tomlplusplus \
       numpy scipy
-    python3 -m pip install --user matplotlib
+    python3 -m pip install --user --break-system-packages matplotlib
     ;;
 
   MINGW*|MSYS*)
