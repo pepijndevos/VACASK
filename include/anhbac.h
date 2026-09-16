712b8fa163a32408642d15197930cc03b30a480d
Árpád Bűrmen <arpad.buermen@gmail.com>
2026-09-16 13:37:49 +0200

    hbac refactored to use anhbsmsig.

--- include/anhbac.h
+++ include/anhbac.h
@@ -1,94 +1,38 @@
 #ifndef __ANHBAC_DEFINED
 #define __ANHBAC_DEFINED
 
-#include <memory>
-#include "an.h"
+#include "anhbsmsig.h"
 #include "corehb.h"
 #include "corehbac.h"
 #include "parameterized.h"
-#include "cscblkmatrix.h"
-#include "solver.h"
 #include "common.h"
 
 
 namespace NAMESPACE {
 
-// HBAC analysis
-class HBAC : public Analysis {
+// HBAC analysis data
+class HBAcData {
 public:
-    typedef HBACParameters Parameters;
-
     static inline const Id analysisId = Id::createStatic("hbac");
 
-    HBAC(Id name, Circuit& circuit, PTAnalysis& ptAnalysis);
-
-    HBAC           (const HBAC&)  = delete;
-    HBAC           (      HBAC&&) = delete;
-    HBAC& operator=(const HBAC&)  = delete;
-    HBAC& operator=(      HBAC&&) = delete;
-
-    virtual Parameterized& parameters() { return params; };
-    virtual const Parameterized& parameters() const { return params; };
-
-    static Analysis* create(PTAnalysis& ptAnalysis, Circuit& circuit, Status& s=Status::ignore);
-
-    virtual void dump(std::ostream& os) const;
-
 protected:
-    virtual bool addCommonOutputDescriptor(const OutputDescriptor& desc);
-    virtual bool addCoreOutputDescriptors(ErrorConsumer& errors);
-    virtual bool resolveSave(const PTSave& save, bool verify, ErrorConsumer& errors);
-    virtual bool addDefaultOutputDescriptors(ErrorConsumer& errors);
-    virtual void clearOutputDescriptors();
-    virtual bool resolveOutputDescriptors(bool strict, ErrorConsumer& errors);
-
-    virtual std::tuple<bool, bool> preMapping(ErrorConsumer& errors);
-    virtual bool populateStructures(ErrorConsumer& errors);
-    virtual bool rebuildCores(ErrorConsumer& errors);
-    virtual bool initializeOutputs(ErrorConsumer& errors);
-
-    virtual AnalysisCore& analysisCore() { return hbacCore; };
-    virtual CoreCoroutine coreCoroutine(bool continuePrevious, ErrorConsumer& errors) {
-        return std::move(hbacCore.coroutine(continuePrevious, errors));
-    };
-
-    virtual bool finalizeOutputs(ErrorConsumer& errors);
-    virtual bool deleteOutputs(ErrorConsumer& errors);
-
-    virtual size_t analysisStateStorageSize() const;
-    virtual size_t allocateAnalysisStateStorage(size_t n);
-    virtual void deallocateAnalysisStateStorage(size_t n=0);
-    virtual bool storeState(size_t ndx, bool storeDetails=true);
-    virtual bool restoreState(size_t ndx);
-    virtual void makeStateIncoherent(size_t ndx);
-
-    // Add HB operating point output descriptor(s), returns ok, handled
-    std::tuple<bool, bool> resolveHbSave(const PTSave& save, bool verify, ErrorConsumer& errors);
-
-    IStruct<HBACParameters> params;
-
-    // Declared before the cores so the references they bind in their init lists
-    // (hbCore: jacColoc/jac/solution; hbacCore: jacSpec/hbSolution/acMatrix/acSolution)
-    // refer to fully-constructed members. hbCore precedes hbacCore because hbacCore
-    // binds a reference to hbCore.
-    CSCBlockSparseRealMatrix jacColoc;
-    CSCBlockSparseRealMatrix jac;
-    VectorRepository<double> solution;
     CSCBlockSparseComplexMatrix jacSpec;
     VectorRepository<Complex> hbSolution;
     CSCBlockSparseComplexMatrix acMatrix;
     Vector<Complex> acSolution;
-
-    DelayLines delayLines_;
-    DelayMatrixBindings<DenseMatrixView<double>> hbDelayBindings_;
-    DelayMatrixBindings<DenseMatrixView<Complex>> hbacDelayBindings_;
-
-    std::unique_ptr<RealSparseSolver> linearSolver_;
-    std::unique_ptr<ComplexSparseSolver> linearCxSolver_;
-
-    HBCore hbCore;
-    HBACCore hbacCore;
 };
+
+// Constructor specialization
+template<> HBSmallSignal<HBACCore, HBAcData>::HBSmallSignal(const std::string& name, Circuit& circuit, PTAnalysis& ptAnalysis);
+
+// Resolve save specialization
+template<> bool HBSmallSignal<HBACCore, HBAcData>::resolveSave(const PTSave& save, bool verify, ErrorConsumer& errors);
+
+// Dump specialization
+template<> void HBSmallSignal<HBACCore, HBAcData>::dump(std::ostream& os) const;
+
+// Typedef HBAC
+typedef HBSmallSignal<HBACCore, HBAcData> HBAC;
 
 }
 
--- lib/anhbac.cpp
+++ lib/anhbac.cpp
@@ -5,17 +5,13 @@
 
 namespace NAMESPACE {
 
-HBAC::HBAC(Id name, Circuit& circuit, PTAnalysis& ptAnalysis)
+template<> HBSmallSignal<HBACCore, HBAcData>::HBSmallSignal(const std::string& name, Circuit& circuit, PTAnalysis& ptAnalysis)
     : Analysis(name, circuit, ptAnalysis),
       hbCore(*this, params.core().hbParams, circuit, commons, jacColoc, jac, solution, delayLines_, hbDelayBindings_),
-      hbacCore(*this, params.core(), hbCore, circuit, commons, jacSpec, hbSolution, acMatrix, acSolution, delayLines_, hbacDelayBindings_) {
+      smsigCore(*this, params.core(), hbCore, circuit, commons, jacSpec, hbSolution, acMatrix, acSolution, delayLines_, smsigDelayBindings_) {
 }
 
-Analysis* HBAC::create(PTAnalysis& ptAnalysis, Circuit& circuit, Status& s) {
-    return new HBAC(ptAnalysis.name(), circuit, ptAnalysis);
-}
-
-bool HBAC::resolveSave(const PTSave& save, bool verify, ErrorConsumer& errors) {
+template<> bool HBSmallSignal<HBACCore, HBAcData>::resolveSave(const PTSave& save, bool verify, ErrorConsumer& errors) {
     static const auto idDefault  = Id("default");
     static const auto idFull     = Id("full");
     static const auto idDv       = Id("dv");
@@ -28,13 +24,13 @@ bool HBAC::resolveSave(const PTSave& save, bool verify, ErrorConsumer& errors) {
     bool st = true;
     bool handled = true;
     if (save.typeName() == idDefault) {
-        st = hbacCore.addAllUnknowns(save, e1);
+        st = smsigCore.addAllUnknowns(save, e1);
     } else if (save.typeName() == idFull) {
-        st = hbacCore.addAllNodes(save, e1);
+        st = smsigCore.addAllNodes(save, e1);
     } else if (save.typeName() == idDv) {
-        st = hbacCore.addNode(save, e1);
+        st = smsigCore.addNode(save, e1);
     } else if (save.typeName() == idDi) {
-        st = hbacCore.addFlow(save, e1);
+        st = smsigCore.addFlow(save, e1);
     } else {
         std::tie(st, handled) = resolveHbSave(save, verify, e1);
         if (!verify) {
@@ -49,197 +45,14 @@ bool HBAC::resolveSave(const PTSave& save, bool verify, ErrorConsumer& errors) {
     return true;
 }
 
-std::tuple<bool, bool> HBAC::resolveHbSave(const PTSave& save, bool verify, ErrorConsumer& errors) {
-    static const auto idHbDefault = Id("hbdefault");
-    static const auto idHbFull    = Id("hbfull");
-    static const auto idV         = Id("v");
-    static const auto idI         = Id("i");
-    static const auto idP         = Id("p");
-
-    ErrorConsumer sink;
-    ErrorConsumer& e1 = verify ? errors : sink;
-
-    bool st = true;
-    if (save.typeName() == idHbDefault) {
-        st = hbCore.addAllUnknowns(save, e1);
-    } else if (save.typeName() == idHbFull) {
-        st = hbCore.addAllNodes(save, e1);
-    } else if (save.typeName() == idV) {
-        st = hbCore.addNode(save, e1);
-    } else if (save.typeName() == idI) {
-        st = hbCore.addFlow(save, e1);
-    } else if (save.typeName() == idP) {
-        st = hbCore.addInstanceOutvar(save, e1);
-    } else {
-        if (verify) {
-            errors.push(AnUnsupportedSaveDirective{save.location()});
-        }
-        return std::make_tuple(false, false);
-    }
-    if (verify && !st) {
-        errors.push(AnSaveDirectiveLocation{save.location()});
-    }
-    return std::make_tuple(st, true);
-}
-
-bool HBAC::addCommonOutputDescriptor(const OutputDescriptor& desc) {
-    bool s1 = hbCore.addOutputDescriptor(desc);
-    bool s2 = hbacCore.addOutputDescriptor(desc);
-    return s1 && s2;
-}
-
-bool HBAC::addCoreOutputDescriptors(ErrorConsumer& errors) {
-    if (!hbCore.addCoreOutputDescriptors(errors)) {
-        return false;
-    }
-    if (!hbacCore.addCoreOutputDescriptors(errors)) {
-        return false;
-    }
-    return true;
-}
-
-bool HBAC::addDefaultOutputDescriptors(ErrorConsumer& errors) {
-    auto s1 = hbCore.addDefaultOutputDescriptors(errors);
-    auto s2 = hbacCore.addDefaultOutputDescriptors(errors);
-    return s1 && s2;
-}
-
-void HBAC::clearOutputDescriptors() {
-    hbCore.clearOutputDescriptors();
-    hbacCore.clearOutputDescriptors();
-}
-
-bool HBAC::resolveOutputDescriptors(bool strict, ErrorConsumer& errors) {
-    if (!hbCore.resolveOutputDescriptors(strict, errors)) {
-        if (strict) {
-            return false;
-        }
-    }
-    if (!hbacCore.resolveOutputDescriptors(strict, errors)) {
-        if (strict) {
-            return false;
-        }
-    }
-    return true;
-}
-
-std::tuple<bool, bool> HBAC::preMapping(ErrorConsumer& errors) {
-    auto [ok, needsMapping] = hbCore.preMapping(errors);
-    if (!ok) {
-        return std::make_tuple(false, needsMapping);
-    }
-    auto [ok1, map1] = hbacCore.preMapping(errors);
-    return std::make_tuple(ok && ok1, needsMapping || map1);
-}
-
-bool HBAC::populateStructures(ErrorConsumer& errors) {
-    if (!hbCore.populateStructures(errors)) {
-        return false;
-    }
-    return hbacCore.populateStructures(errors);
-}
-
-bool HBAC::rebuildCores(ErrorConsumer& errors) {
-    if (!hbCore.rebuild(errors)) {
-        return false;
-    }
-
-    auto& options = circuit.simulatorOptions().core();
-
-    // Large-signal HB linear solver, built on jac by hbCore.rebuild()
-    if (params.core().hbParams.solve) {
-        auto solverId = params.core().hbParams.solver;
-        solverId = solverId?solverId:options.hbsolver;
-        solverId = solverId?solverId:Simulator::defaultHbSolverId;
-        linearSolver_ = std::unique_ptr<RealSparseSolver>(RealSparseSolver::createSolver(solverId, jac, errors));
-        if (!linearSolver_) {
-            return false;
-        }
-        // jac has nt x nt dense blocks; hint the solver before it builds
-        linearSolver_->setBlockSize(jac.nBlockElementCols());
-        if (!linearSolver_->rebuild(errors)) {
-            return false;
-        }
-        hbCore.setLinearSolver(linearSolver_.get());
-    }
-
-    if (!hbacCore.rebuild(errors)) {
-        return false;
-    }
-
-    // Small-signal conversion-matrix solver (acMatrix pattern built by hbacCore.rebuild())
-    auto cxSolverId = params.core().solver;
-    cxSolverId = cxSolverId?cxSolverId:options.qpsmsigsolver;
-    cxSolverId = cxSolverId?cxSolverId:Simulator::defaultQpsmsigSolverId;
-    linearCxSolver_ = std::unique_ptr<ComplexSparseSolver>(
-        ComplexSparseSolver::createSolver(cxSolverId, acMatrix, errors));
-    if (!linearCxSolver_) {
-        return false;
-    }
-    // acMatrix has nf x nf dense blocks; hint the solver before it builds
-    linearCxSolver_->setBlockSize(acMatrix.nBlockElementCols());
-    if (!linearCxSolver_->rebuild(errors)) {
-        return false;
-    }
-    hbacCore.setLinearSolver(linearCxSolver_.get());
-
-    return true;
-}
-
-bool HBAC::initializeOutputs(ErrorConsumer& errors) {
-    if (!hbCore.initializeOutputs(prefixedName_+".hb", errors)) {
-        return false;
-    }
-    if (!hbacCore.initializeOutputs(prefixedName_, errors)) {
-        return false;
-    }
-    return true;
-}
-
-bool HBAC::finalizeOutputs(ErrorConsumer& errors) {
-    auto ok1 = hbCore.finalizeOutputs(errors);
-    auto ok2 = hbacCore.finalizeOutputs(errors);
-    return ok1 && ok2;
-}
-
-bool HBAC::deleteOutputs(ErrorConsumer& errors) {
-    auto ok1 = hbCore.deleteOutputs(prefixedName_+".hb", errors);
-    auto ok2 = hbacCore.deleteOutputs(prefixedName_, errors);
-    return ok1 && ok2;
-}
-
-size_t HBAC::analysisStateStorageSize() const {
-    return hbCore.stateStorageSize();
-}
-
-size_t HBAC::allocateAnalysisStateStorage(size_t n) {
-    return hbCore.allocateStateStorage(n);
-}
-
-void HBAC::deallocateAnalysisStateStorage(size_t n) {
-    hbCore.deallocateStateStorage(n);
-}
-
-bool HBAC::storeState(size_t ndx, bool storeDetails) {
-    return hbCore.storeState(ndx, storeDetails);
-}
-
-bool HBAC::restoreState(size_t ndx) {
-    return hbCore.restoreState(ndx);
-}
-
-void HBAC::makeStateIncoherent(size_t ndx) {
-    hbCore.makeStateIncoherent(ndx);
-}
-
-void HBAC::dump(std::ostream& os) const {
+template<> void HBSmallSignal<HBACCore, HBAcData>::dump(std::ostream& os) const {
     Analysis::dump(os);
     os << "Analysis type: HBAC (quasi)periodic small-signal\n";
     os << "HB analysis core:\n";
     hbCore.dump(os);
     os << "\n";
     os << "HBAC small-signal analysis core:\n";
-    hbacCore.dump(os);
+    smsigCore.dump(os);
 }
 
 }
